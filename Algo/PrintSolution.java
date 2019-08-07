3420c874dcc174844ad2dfab28129ae3061fb064
Spore <Spore@175.197.81.238>
2019-08-07 11:14:02 +0900

    프로그래머스 코딩테스트 연습 > 연습문제 > 수박수박수박수박수박수?

---
+++ Algo/PrintSolution.java
@@ -0,0 +1,16 @@
+package Algo;
+
+public class PrintSolution {
+	
+	public int Solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;		
+	}
+
+	public static void main(String[] args) {
+		PrintSolution ps = new PrintSolution();
+		int[] priorities = new int[] {2,1,3,2};
+		ps.Solution(priorities, 2);
+	}
+
+}
