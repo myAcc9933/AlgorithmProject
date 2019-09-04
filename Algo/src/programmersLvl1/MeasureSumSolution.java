3ddac14507c880910b1424ae7127ebb7693f15f8
Wakestand <wakeisle9933@gmail.com>
2019-09-04 22:22:32 +0900

    2019/09/04 프로그래머스 > 코딩테스트 연습 > 연습문제 > 약수의 합

--- Algo/src/programmersLvl1/MeasureSumSolution.java
+++ Algo/src/programmersLvl1/MeasureSumSolution.java
@@ -4,7 +4,7 @@ package programmersLvl1;
 //프로그래머스 > 코딩테스트 연습 > 연습문제 > 약수의 합
 public class MeasureSumSolution {
 	  public int solution(int n) {
-	      int answer = 0;
+	      int answer = 0; 
 	      
 	      for(int i = 1; i<=n; i++) {   // for문을 n값까지 진행
 	    	  if(n % i == 0) { 			   // n을 i로 나눠 나머지 값이 없다면
@@ -17,7 +17,7 @@ public class MeasureSumSolution {
 
 	public static void main(String[] args) {
 		MeasureSumSolution mss = new MeasureSumSolution();
-		System.out.println(mss.solution(5));
+		System.out.println(mss.solution(6));
 	}
 
 }
